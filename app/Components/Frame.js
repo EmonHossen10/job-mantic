5458e3314c06b931b0a7ad5ef8ed8e2ac5d1523e
EmonHossen10 <emonhossen1000@gmail.com>
2025-09-25 12:55:46 +0600

    grame added to desktop mode

--- app/Components/Frame.js
+++ app/Components/Frame.js
@@ -1,14 +1,31 @@
-import React from "react";
+import Image from "next/image";
+import img1 from "@/public/frame1.png";
+import img2 from "@/public/frame2.png";
+import img3 from "@/public/frame3.png";
+import bg from "@/public/gradient.png";
 
 const Frame = () => {
   return (
-    <div>
+    <div
+      className="bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
       <h2 className="text-2xl text-center py-10">
         You are
-        <span className="font-bold text-[#23A2FC]">90% more likely</span> to get
-        hired if you use AI Apply
+        <span className="font-bold text-[#23A2FC]"> 90% more likely</span> to
+        get hired if you use AI Apply
       </h2>
-      
+      <section className="flex justify-center pb-5 gap-5">
+        <div className="p-5">
+          <Image src={img1} alt="Frame 1" />
+        </div>
+        <div className="p-5">
+          <Image src={img2} alt="Frame 2" />
+        </div>
+        <div className="p-5">
+          <Image src={img3} alt="Frame 3" />
+        </div>
+      </section>
     </div>
   );
 };
