78309eaec6fe19a5eb576aef97d35a68d32e637d
EmonHossen10 <emonhossen1000@gmail.com>
2025-09-25 12:37:34 +0600

    frame added

--- app/Components/Frame.js
+++ app/Components/Frame.js
@@ -3,8 +3,8 @@ import React from "react";
 const Frame = () => {
   return (
     <div>
-      <h2 className="text-2xl">
-        You are{" "}
+      <h2 className="text-2xl text-center py-10">
+        You are
         <span className="font-bold text-[#23A2FC]">90% more likely</span> to get
         hired if you use AI Apply
       </h2>
